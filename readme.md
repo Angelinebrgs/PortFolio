6bb8d49b75d6534822caa246758e98c78442b6ed
Angéline <titou.borges@gmail.com>
2023-09-19 18:49:20 +0200

    header reste les ancres a mettre

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-Hello this will be another portfolio i'll build using vite to config and some smoother styles i'm trying to improve.
+Hello,
+ this will be another portfolio i'll build using vite to config and some smoother styles i'm trying to improve.
+
+ if you want to see it you can take it and to run it you need to have node js installed
+
+ the command to run the project is npm run dev
--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 import '../style/modern-normalise.css';
 import '../style/style.css';
 import '../style/utils.css'
+
+//Pour que des élément apparaisse au fur a mesure avec animation
+const observer = new IntersectionObserver(() => {
+    entries.forEach(() => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el => observer.observe(el)));
